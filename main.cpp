d1f41b93007ae223bb21dc1417ead3998b83096a
KenyaTamura <denn_sonn@yahoo.co.jp>
2017-11-02 15:58:22 +0900

    time measurement

--- main.cpp
+++ main.cpp
@@ -21,7 +21,6 @@ namespace {
 	Data* q = nullptr;
 	int threshold = 0;
 	string ofname;
-	string ofname_pre;
 	string type = "quad";
 	bool cmp_flag = false;
 	bool gpu_flag = true;
@@ -31,7 +30,7 @@ void arg_branch(int argc, char* argv[]) {
 	auto cmp = [&](int i, const char* str) -> bool {return (strcmp(argv[i], str) == 0); };
 	for (int i = 1; i < argc; ++i) {
 		if (cmp(i,"-convert")) {
-			if (i + 2 < argc) { FileConverter f(argv[i+1],argv[i+2]); }
+			if (i + 2 < argc) { FileConverter f(argv[i+1], argv[i+2]); }
 			i += 2;
 		}
 		if (cmp(i, "-db")) {
@@ -43,12 +42,6 @@ void arg_branch(int argc, char* argv[]) {
 		if (cmp(i, "-t")) {
 			if (++i < argc) { threshold = atoi(argv[i]); }
 		}
-		if (cmp(i, "-o")) {
-			if (++i < argc) { ofname = argv[i]; }
-		}
-		if (cmp(i, "-predebug")) {
-			if (++i < argc) { ofname_pre = argv[i]; }
-		}
 		if (cmp(i, "-type")) {
 			if (++i < argc) { type = argv[i]; }
 		}
@@ -58,17 +51,18 @@ void arg_branch(int argc, char* argv[]) {
 		if (cmp(i, "-cpu")){
 			gpu_flag = false;
 		}
+		if(cmp(i, "-time")){
+			if(++i < argc) { ofname = argv[i]; }
+		}
 	}
 }
 
-int main(int argc, char* argv[]) {
-	arg_branch(argc, argv);
+void mode_select(){
 	auto type_check = [&](const char* str) -> bool {return (strcmp(type.c_str(), str) == 0); };
-	Timer t;
 	if (db != nullptr && q != nullptr && db->data() != nullptr && q->data() != nullptr) {
 		// Compare execution time
 		if (cmp_flag) {
-			t.start();
+			Timer t;
 			PreprocessSW(*db, *q, PreprocessSingle(*db, *q, threshold), threshold);
 			cout << t.get_millsec() << endl;
 			t.start();
@@ -78,19 +72,10 @@ int main(int argc, char* argv[]) {
 			SimpleSW(*db, *q, threshold);
 			cout << t.get_millsec() << endl;
 		}
-		else if (!ofname_pre.empty()) {
-			if (type_check("single")) { PreprocessSingle(*db, *q, ofname_pre.c_str()); }
-			else if (type_check("quad")) { PreprocessQuad(*db, *q, ofname_pre.c_str()); }
-		}
 		// All of result at preprocess
 		else {
 			if (type_check("simple")) { 
 				SimpleSW sw(*db, *q, threshold);
-				// All of result
-				if (!ofname.empty()) {
-					Writer w;
-					w.writing_score(ofname.c_str(), sw.all_score(), db->size(), db->size() / 100);
-				}
 			}
 			else if(gpu_flag){
 				if (type_check("single")) {
@@ -111,7 +96,20 @@ int main(int argc, char* argv[]) {
 			}
 		}
 	}
+	else{
+		cout << "File error" << endl;	
+	}
+}
+
+int main(int argc, char* argv[]) {
+	arg_branch(argc, argv);
+	Timer t;
+	mode_select();
 	cout << t.get_millsec() << endl;
+	if(!ofname.empty()){
+		Writer w;
+		w.writing_time(ofname.c_str(), t.get_millsec());
+	}
 	if (db) {
 		delete db;
 	}
